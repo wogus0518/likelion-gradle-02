5dfdb2acad37ec7f2eaee27a9ca8d244d873d4e6
JaeHyun <wogus0518@naver.com>
2022-10-20 12:55:18 +0900

    first commit

---
+++ src/main/java/codelion/likeliongradle/LikelionGradleApplication.java
@@ -0,0 +1,13 @@
+package codelion.likeliongradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codelion/likeliongradle/LikelionGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package codelion.likeliongradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
