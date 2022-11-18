b3f99f6e9b486bb5dc13a87275da53c1f60cf821
JaeHyun <wogus0518@naver.com>
2022-11-18 10:28:20 +0900

    프로그래머스 : 시저 암호 메서드 추출

--- src/main/java/codelion/likeliongradle/algorithm/programmers/CaesarCipher.java
+++ src/main/java/codelion/likeliongradle/algorithm/programmers/CaesarCipher.java
@@ -3,38 +3,36 @@ package codelion.likeliongradle.algorithm.programmers;
 public class CaesarCipher {
 
     //a-z => 97-122
-    //A-z => 65-90
+    //A-Z => 65-90
     //공백 => 32
     private static String solution(String s, int n) {
         String answer = "";
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+
             if (c == ' ') {
                 answer += ' ';
-            } else {
-                //대문자일 때
-                if (65 <= c && c <= 90) {
-                    char cipher = (char) (c + n);
-                    if (65 <= cipher && cipher <= 90) {
-                        answer += cipher;
-                    } else {
-                        answer += (char) (cipher-26);
-                    }
-                } else if (97 <= c && c <= 122) {
-                    char cipher = (char) (c + n);
-                    if (97 <= cipher && cipher <= 122) {
-                        answer += cipher;
-                    } else {
-                        answer += (char) (cipher-26);
-                    }
-                }
+                continue;
             }
+
+            if (Character.isUpperCase(c)) answer = addCipher(65, 90, answer, c, n);
+            else if (Character.isLowerCase(c)) answer = addCipher(97, 122, answer, c, n);
         }
         return answer;
     }
 
+    private static String addCipher(int start, int end, String answer, char c, int n) {
+        char cipher = (char) (c + n);
+
+        if (start <= cipher && cipher <= end) answer += cipher;
+        else answer += (char) (cipher - 26);
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        String result = solution("a B Z"	, 8);
+        String result = solution("a B Z", 8);
         System.out.println(result);
     }
 }
