076faa1e847e0c45033366b7820fdde00edbd54d
JaeHyun <wogus0518@naver.com>
2022-12-08 09:32:09 +0900

    피보나치 수열 : DP 적용

--- src/main/java/codelion/likeliongradle/algorithm/recursion/Fibonacci.java
+++ src/main/java/codelion/likeliongradle/algorithm/recursion/Fibonacci.java
@@ -1,21 +1,38 @@
 package codelion.likeliongradle.algorithm.recursion;
 
 public class Fibonacci {
-    public static int fib(int n) {
+    //재귀 적용
+    public static int recur(int n) {
         if (n <= 1) {
             return n;
         } else {
-            return fib(n - 1) + fib(n - 2);
+            return recur(n - 1) + recur(n - 2);
         }
     }
 
+    //DP 적용
+    public static int dp(int n) {
+        int[] memo = new int[100];
+        memo[0] = 0;
+        memo[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n];
+    }
+
     public static void main(String[] args) {
-        System.out.println(fib(0) == 0);
-        System.out.println(fib(1) == 1);
-        System.out.println(fib(2) == 1);
-        System.out.println(fib(3) == 2);
-        System.out.println(fib(4) == 3);
-        System.out.println(fib(5) == 5);
-        System.out.println(fib(6) == 8);
+        System.out.println("피보나치 수열 - 재귀로 실행");
+        long start = System.currentTimeMillis();
+        System.out.println(recur(45));
+        long end = System.currentTimeMillis();
+        System.out.println( "실행 시간 : " + ( end - start )/1000.0 +"초"); //최종 실행시간
+        System.out.println();
+
+        System.out.println("피보나치 수열 - 동적 프로그래밍으로 실행");
+        start = System.currentTimeMillis();
+        System.out.println(dp(45));
+        end = System.currentTimeMillis();
+        System.out.println( "실행 시간 : " + ( end - start )/1000.0 +"초"); //최종 실행시간
     }
 }
